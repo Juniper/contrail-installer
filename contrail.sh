adb735bbd147f5b7e46e92ad45359e8a400fbf0e
Frank Borkin <borkinfr@gmail.com>
2014-02-17 14:32:53 +0200

    Add missing dep

--- contrail.sh
+++ contrail.sh
@@ -24,7 +24,7 @@ function install_contrail() {
 
     # dependencies
     if is_ubuntu; then
-	apt_get install patch scons flex bison make vim
+	apt_get install patch scons flex bison make vim unzip
 	apt_get install libexpat-dev libgettextpo0 libcurl4-openssl-dev
 	apt_get install python-dev autoconf automake build-essential libtool
 	apt_get install libevent-dev libxml2-dev libxslt-dev
