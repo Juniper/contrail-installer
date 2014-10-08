8a7662f671a42c78f9c053d0e913f4a02ecad07c
Anonymous <anonymous@nowhere.com>
2014-10-08 22:00:20 +0530

    modified status check based on enabled_services in task.sh,made sanity.sh as standalone

--- utilities/sanity.sh
+++ utilities/sanity.sh
@@ -9,7 +9,9 @@
 #
 
 
-
+CLONE_DIR=${CLONE_DIR:-`pwd`}
+run_sanity=${run_sanity:-"True"}
+WITH_CONTRAIL_CLONE=${WITH_CONTRAIL_CLONE:-True}
 NETWORK_NAME=${NETWORK_NAME:-net}
 SUBNET_NAME=${SUBNET_NAME:-subnet}
 SUBNET_CIDR=${SUBNET_CIDR:-11.0.0.0/24}
@@ -20,8 +22,34 @@ VM_USER=${VM_USER:-cirros}
 VM_PASSWORD=${VM_PASSWORD:-cubswin:)}
 #CLONE_DIR=`pwd`
 _CONTRAIL_NEUTRON_SERVICES=("apiSrv" "schema" "svc-mon") 
-source $DEVSTACK_DIR/openrc $TENANT_NAME $TENANT_NAME
+#source $DEVSTACK_DIR/openrc $TENANT_NAME $TENANT_NAME
 
+
+function set_environment()
+{
+    if [[ "$WITH_CONTRAIL_CLONE" = "False" ]]; then
+        CONTRAIL_CLONE_DIR=`pwd`
+        CONTRAIL_DIR=$CONTRAIL_CLONE_DIR/../../contrail-installer
+        DEVSTACK_CLONE_DIR=$CONTRAIL_DIR/..
+        DEVSTACK_DIR=$DEVSTACK_CLONE_DIR/devstack
+    else
+        CONTRAIL_CLONE_DIR=$CLONE_DIR
+        CONTRAIL_DIR=$CLONE_DIR/contrail-installer
+        DEVSTACK_CLONE_DIR=$CLONE_DIR
+        DEVSTACK_DIR=$DEVSTACK_CLONE_DIR/devstack
+    fi
+}
+
+if [[ "$run_sanity" = "True" ]] ; then
+    set_environment
+fi
+
+echo "$CONTRAIL_DIR"
+if [[ -f $DEVSTACK_DIR/openrc ]] ; then
+    source $DEVSTACK_DIR/openrc $TENANT_NAME $TENANT_NAME
+else
+    echo "Invalid devstack directory please set DEVSTACK_DIR and try again"
+fi
 #report generation variables
 test_cases=("ServiceStatus" "ServiceRequest" "NeutronRequest" "VmCreation" "SimpleGateway" "PingBeweenVms")
 
@@ -436,3 +464,7 @@ expect -c "
        echo "TESTCASE	PING FROM $source_ip TO $destination_ip    :       FAILED" >> $report_file
    fi
 }
+
+if [[ "$run_sanity" = "True" ]]; then
+    start_sanity_script
+fi
