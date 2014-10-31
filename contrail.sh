a1a855a01ade53d09d5c2313bd4b7df9e5a3e7b3
Deepinder Setia <dsetia@juniper.net>
2014-10-31 12:24:42 -0700

    Switch to contrail-control as newer packages get uploaded to launchpad

--- contrail.sh
+++ contrail.sh
@@ -862,10 +862,8 @@ function start_contrail() {
         #source /etc/contrail/control_param.conf
         if [[ "$CONTRAIL_DEFAULT_INSTALL" != "True" ]]; then
             screen_it control "export LD_LIBRARY_PATH=/opt/stack/contrail/build/lib; $CONTRAIL_SRC/build/production/control-node/contrail-control --conf_file /etc/contrail/contrail-control.conf ${CERT_OPTS} ${LOG_LOCAL}"
-        elif [[ "$LAUNCHPAD_BRANCH" = "PPA" ]]; then
-            screen_it control "export LD_LIBRARY_PATH=/usr/lib; /usr/bin/contrail-control --conf_file /etc/contrail/contrail-control.conf ${CERT_OPTS} ${LOG_LOCAL}"
         else
-            screen_it control "export LD_LIBRARY_PATH=/usr/lib; /usr/bin/control-node --conf_file /etc/contrail/contrail-control.conf ${CERT_OPTS} ${LOG_LOCAL}"
+            screen_it control "export LD_LIBRARY_PATH=/usr/lib; /usr/bin/contrail-control --conf_file /etc/contrail/contrail-control.conf ${CERT_OPTS} ${LOG_LOCAL}"
         fi
 
         # collector/vizd
